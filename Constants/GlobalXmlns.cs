646c680eebdc893cde08e723f205fcb914408104
Eduardo Gomez <egomezr@outlook.com>
2026-02-02 20:32:47 +0100

    Create a style for the combobox

--- Constants/GlobalXmlns.cs
+++ Constants/GlobalXmlns.cs
@@ -1,45 +1,12 @@
-﻿// =======================
-// YOUR APP
-// =======================
-[assembly: XmlnsDefinition(
-    "http://schemas.textilesystem.com/maui",
-    "TextileSystem.Controls")]
-[assembly: XmlnsDefinition(
-    "http://schemas.textilesystem.com/maui",
-    "TextileSystem.ViewModels")]
-[assembly: XmlnsDefinition(
-    "http://schemas.textilesystem.com/maui",
-    "TextileSystem.MenuViewModels")]
-[assembly: XmlnsDefinition(
-    "http://schemas.textilesystem.com/maui",
-    "TextileSystem.Helpers")]
-[assembly: XmlnsDefinition(
-    "http://schemas.textilesystem.com/maui",
-    "TextileSystem.Models")]
-[assembly: XmlnsPrefix(
-    "http://schemas.textilesystem.com/maui",
-    "app")]
+﻿[assembly: XmlnsDefinition("http://schemas.textilesystem.com/maui", "TextileSystem.Controls")]
+[assembly: XmlnsDefinition("http://schemas.textilesystem.com/maui", "TextileSystem.ViewModels")]
+[assembly: XmlnsDefinition("http://schemas.textilesystem.com/maui", "TextileSystem.MenuViewModels")]
+[assembly: XmlnsDefinition("http://schemas.textilesystem.com/maui", "TextileSystem.Helpers")]
+[assembly: XmlnsDefinition("http://schemas.textilesystem.com/maui", "TextileSystem.Models")]
+[assembly: XmlnsPrefix("http://schemas.textilesystem.com/maui", "app")]
 
-// =======================
-// SYNCFUSION
-// =======================
-
-[assembly: XmlnsDefinition(
-    "http://schemas.syncfusion.com/maui",
-    "Syncfusion.Maui.Core")]
-
-[assembly: XmlnsDefinition(
-    "http://schemas.syncfusion.com/maui",
-    "Syncfusion.Maui.Inputs")]
-
-[assembly: XmlnsDefinition(
-    "http://schemas.syncfusion.com/maui",
-    "Syncfusion.Maui.Popup")]
-
-[assembly: XmlnsDefinition(
-    "http://schemas.syncfusion.com/maui",
-    "Syncfusion.Maui.TabView")]
-
-[assembly: XmlnsPrefix(
-    "http://schemas.syncfusion.com/maui",
-    "sf")]
+[assembly: XmlnsDefinition("http://schemas.syncfusion.com/maui", "Syncfusion.Maui.Core")]
+[assembly: XmlnsDefinition("http://schemas.syncfusion.com/maui", "Syncfusion.Maui.Inputs")]
+[assembly: XmlnsDefinition("http://schemas.syncfusion.com/maui", "Syncfusion.Maui.Popup")]
+[assembly: XmlnsDefinition("http://schemas.syncfusion.com/maui", "Syncfusion.Maui.TabView")]
+[assembly: XmlnsPrefix("http://schemas.syncfusion.com/maui", "sf")]
