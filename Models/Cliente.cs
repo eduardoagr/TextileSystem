4937ec8271b5be433491d65008ac29c38dbdc06e
pcmendez <pcruz@crumen.com.com>
2026-03-13 08:50:25 -0600

    PCM - Se agregan cambios
    Se agregan los modelos de clientes, proveedores, productos y colores, usuarios

---
+++ Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿namespace TextileSystem.Models
+{
+    public class Cliente
+    {
+
+        public Int64? cl_id { get; set; }
+        public string cl_nombre { get; set; }
+
+        public string cl_direccion { get; set; }
+
+        public string cl_calle { get; set; }
+        public string cl_numExt { get; set; }
+        public string cl_numInt { get; set; }
+        public string cl_colonia { get; set; }
+        public string cl_delegacion { get; set; }
+        public string cl_estado { get; set; }
+        public string cl_cp { get; set; }
+
+        public string cl_telefonos { get; set; }
+        public string cl_observaciones { get; set; }
+        public bool? cl_status { get; set; }
+
+
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextileSystem.Models
+{
+    public class Producto
+    {
+
+        public Int64 prod_id { get; set; }
+        public string prod_nombre { get; set; }
+
+        public decimal? prod_precioMen { get; set; }
+        public decimal? prod_precioRollo { get; set; }
+        public decimal? prod_precioMay { get; set; }
+
+        public decimal? prod_precioMenT { get; set; }
+        public decimal? prod_precioRolloT { get; set; }
+        public decimal? prod_precioMayT { get; set; }
+
+        public decimal? prod_costoprom { get; set; }
+        public decimal? prod_ultimocosto { get; set; }
+        public decimal? prod_total { get; set; }
+        public string prod_utlidadPorc { get; set; }
+        public string prod_utlidadPorcUc { get; set; }
+
+        public bool prod_cap { get; set; }
+        public bool prod_fashion { get; set; }
+
+        public bool? prod_status { get; set; }
+
+        public decimal? inv_cantidad { get; set; }
+
+    }
+}
